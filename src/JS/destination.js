7d9f3a42073655dcf8037520db876fd94d53fff2
akashdhakad <akashdhakad043@gmail.com>
2025-12-23 10:13:19 +0530

    day3

--- src/JS/destination.js
+++ src/JS/destination.js
@@ -3,6 +3,15 @@ const params = new URLSearchParams(window.location.search);
 const city = params.get("city");
 
 const cityName = document.getElementById("cityName");
+const loading = document.querySelector("#loading");
+const cityInfoBox = document.querySelector("#cityInfoBox");
+const errorBox = document.querySelector("#error");
+
+const lat = document.querySelector("#lat");
+const lon = document.querySelector("#lon");
+const country = document.querySelector("#country");
+const state = document.querySelector("#state");
+const population = document.querySelector("#population");
 
 let GEODB_API_KEY = "f83d61f4dcmsh1977e04dc26ee2bp12c91ajsn23b7bc374f7e";
 let GEODB_HOST = "wft-geo-db.p.rapidapi.com";
@@ -33,11 +42,24 @@ if (city) {
       }
       let cityData = data.data[0];
       console.log(cityData);
+
+      lat.textContent = cityData.latitude;
+      lon.textContent = cityData.longitude;
+      country.textContent = cityData.country;
+      state.textContent = cityData.region || "N/A";
+      population.textContent = cityData.population
+        ? cityData.population.toLocaleString()
+        : "N/A";
+
+      loading.classList.add("hidden");
+      cityInfoBox.classList.remove("hidden");
     } catch (error) {
       console.log(error);
+      loading.classList.add("hidden");
+      errorBox.classList.remove("hidden");
     }
   }
-  
+
   fetchCityDetails(city);
 } else {
   cityName.textContent = "Unknown City";
